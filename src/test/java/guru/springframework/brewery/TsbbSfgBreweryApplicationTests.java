3e4a110664b7a57a0ca922b3121efb828756685d
PierreSQS <nomo_messina@web.de>
2021-06-26 15:14:18 +0200

    Chap174-Updating to Junit5: Introduced Junit5

--- src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
+++ src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
@@ -1,16 +1,17 @@
 package guru.springframework.brewery;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.context.ApplicationContext;
 
-@RunWith(SpringRunner.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
-public class TsbbSfgBreweryApplicationTests {
+class TsbbSfgBreweryApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads(ApplicationContext appCtx) {
+        assertThat(appCtx).isNotNull();
     }
 
 }
