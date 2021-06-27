b3d42ac85fb87743931db6ed4b9ef148b2e51446
PierreSQS <nomo_messina@web.de>
2021-06-28 01:11:30 +0200

    Chap182-JSonPath with List: Added Test for listBeers()

--- src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTest.java
@@ -1,20 +1,25 @@
 package guru.springframework.brewery.web.controllers;
 
-import guru.springframework.brewery.domain.Beer;
 import guru.springframework.brewery.services.BeerService;
 import guru.springframework.brewery.web.model.BeerDto;
+import guru.springframework.brewery.web.model.BeerPagedList;
+import guru.springframework.brewery.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -35,7 +40,39 @@ class BeerControllerTest {
     MockMvc mockMvc;
 
     @Test
-    void listBeers() {
+    void listBeers() throws Exception {
+        // Given
+        String beerNameMock = "Beer Mock";
+        BeerStyleEnum beerStyleEnumMock = BeerStyleEnum.PILSNER;
+        Integer pageNumberMock = 1;
+        Integer pageSizeMock = 3;
+
+        BeerDto beerDtoMock1 = new BeerDto();
+        beerDtoMock1.setBeerName("Beer Mock1");
+        beerDtoMock1.setBeerStyle(BeerStyleEnum.PILSNER);
+
+        BeerDto beerDtoMock2 = new BeerDto();
+        beerDtoMock2.setBeerName("Beer Mock2");
+
+        List<BeerDto> beerDtoListMock = new ArrayList<>(Arrays.asList(beerDtoMock1,beerDtoMock2));
+        BeerPagedList beerPagedListMock = new BeerPagedList(beerDtoListMock,PageRequest.of(pageNumberMock,pageSizeMock),2);
+//        Works also
+//        BeerPagedList beerPagedListMock = new BeerPagedList(beerDtoListMock);
+
+        when(beerServiceMock.listBeers(beerNameMock, BeerStyleEnum.PILSNER,
+                PageRequest.of(pageNumberMock,pageSizeMock))).thenReturn(beerPagedListMock);
+
+        mockMvc.perform(get("/api/v1/beers")
+                    .param("pageNumber",Integer.toString(pageNumberMock))
+                    .param("pageSize",Integer.toString(pageSizeMock))
+                    .param("beerName",beerNameMock)
+                    .param("beerStyle",BeerStyleEnum.PILSNER.toString()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].beerStyle", equalTo("PILSNER")))
+                .andExpect(jsonPath("$.pageable.sort.unsorted",equalTo(true)))
+                .andExpect(jsonPath("$.pageable.offset",equalTo(3)))
+                .andExpect(jsonPath("$.numberOfElements",equalTo(2)))
+                .andDo(print());
     }
 
     @Test
