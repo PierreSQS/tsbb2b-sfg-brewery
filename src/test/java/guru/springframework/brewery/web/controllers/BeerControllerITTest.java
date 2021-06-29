e668410a4acb0d7596e3beba428071b8f4c5531b
PierreSQS <nomo_messina@web.de>
2021-06-30 00:14:59 +0200

    Chap185-Using TestRestTemplate: Fixed the integration test

--- src/test/java/guru/springframework/brewery/web/controllers/BeerControllerITTest.java
+++ src/test/java/guru/springframework/brewery/web/controllers/BeerControllerITTest.java
@@ -1,12 +1,18 @@
 package guru.springframework.brewery.web.controllers;
 
+import guru.springframework.brewery.web.model.BeerDto;
+import guru.springframework.brewery.web.model.BeerPagedList;
+import guru.springframework.brewery.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BeerControllerITTest {
 
     @Autowired
@@ -17,7 +23,15 @@ class BeerControllerITTest {
     }
 
     @Test
+    @DisplayName("integration test list Beers")
     void testListBeers() {
+        BeerDto expectedBeerDto = new BeerDto();
+        expectedBeerDto.setBeerName("Mango Bobs");
+        expectedBeerDto.setBeerStyle(BeerStyleEnum.IPA);
+        expectedBeerDto.setUpc(337010000001L);
+        BeerPagedList beerPagedList = testRestTemplate.getForObject("/api/v1/beers/", BeerPagedList.class);
+        assertThat(beerPagedList.stream().findFirst().get().getBeerName()).isEqualTo(expectedBeerDto.getBeerName());
 
+        System.out.println(beerPagedList);
     }
 }
