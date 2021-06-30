54a1d03e3b03184e310abf727b0cdf8c59ac5f6d
PierreSQS <nomo_messina@web.de>
2021-06-30 14:04:21 +0200

    Chap186-Assignment: added the test BeerOrderController.getOrder()

--- src/test/java/guru/springframework/brewery/web/controllers/BeerOrderControllerTest.java
+++ src/test/java/guru/springframework/brewery/web/controllers/BeerOrderControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.UUID;
 
-import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -47,6 +46,29 @@ class BeerOrderControllerTest {
 
     @Test
     void getOrder() throws Exception {
+        // Given
+        CustomerDto customerDtoMock = CustomerDto.builder()
+                .id(UUID.fromString("4793b3fc-0f52-485f-910c-7915a8a4e312"))
+                .build();
+
+        BeerOrderDto beerOrderDtoMock = BeerOrderDto.builder()
+                .id(UUID.fromString("a6935dc7-9adb-421b-b9d7-eb76c6271f6e"))
+                .customerId(customerDtoMock.getId())
+                .orderStatus(OrderStatusEnum.NEW)
+                .build();
+
+        when(beerOrderSrvMock.getOrderById(customerDtoMock.getId(),beerOrderDtoMock.getId()))
+                .thenReturn(beerOrderDtoMock);
+
+        // When, Then
+        final String customerID = customerDtoMock.getId().toString();
+        final String orderID = beerOrderDtoMock.getId().toString();
+        ResultActions perform = mockMvc.perform(get(BASEURL+ customerID +"/orders/"+ orderID));
+        perform.andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.customerId",equalTo(beerOrderDtoMock.getCustomerId().toString())))
+                .andExpect(jsonPath("$.orderStatus",equalTo("NEW")))
+                .andDo(print());
     }
 
     @Test
