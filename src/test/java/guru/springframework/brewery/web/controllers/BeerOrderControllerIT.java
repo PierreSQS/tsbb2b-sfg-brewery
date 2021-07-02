7eee19768fb870905a84d9722307f8d9e15fe0a8
PierreSQS <nomo_messina@web.de>
2021-07-02 12:38:17 +0200

    Chap186-Assignment NOK: made the integration test PASS (s commit 19dcb667)

--- src/test/java/guru/springframework/brewery/web/controllers/BeerOrderControllerIT.java
+++ src/test/java/guru/springframework/brewery/web/controllers/BeerOrderControllerIT.java
@@ -1,6 +1,9 @@
 package guru.springframework.brewery.web.controllers;
 
+import guru.springframework.brewery.domain.Customer;
+import guru.springframework.brewery.repositories.CustomerRepository;
 import guru.springframework.brewery.web.model.BeerOrderPagedList;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,9 +16,20 @@ class BeerOrderControllerIT {
     @Autowired
     TestRestTemplate testRestTemp;
 
+    @Autowired
+    CustomerRepository customerRepo;
+
+    Customer cust;
+
+    @BeforeEach
+    void setUp() {
+        cust = customerRepo.findAll().get(0);
+    }
+
     @Test
     void testListOrders() {
-        String testUrl = "/api/v1/customers/74bf4163-e2f7-405c-921c-b602f4a28f8a/orders";
+        String testUrl = "/api/v1/customers/"+cust.getId()+"/orders";
+        System.out.printf("##### %nthe test URL %s%n%n####",testUrl);
         BeerOrderPagedList beerOrderPagedList = testRestTemp.getForObject(testUrl, BeerOrderPagedList.class);
         assertThat(beerOrderPagedList.getContent()).hasSize(1);
     }
