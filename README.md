64fbf9a6d53556e4e46e3f23d58ab83e2d7f2233
PierreSQS <nomo_messina@web.de>
2022-03-03 02:37:33 +0100

    Sec16-Chap177-Ass Review: added assertion in test and CircleCI config and build badge

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Testing Spring Boot B2G - SFG Brewery
+[![CircleCI](https://circleci.com/gh/PierreSQS/tsbb2b-sfg-brewery/tree/circleci.svg?style=svg)](https://circleci.com/gh/PierreSQS/tsbb2b-sfg-brewery/tree/circleci)
 
 All source code examples in the repository are for my [Online Course - Testing Spring Beginner to Guru](https://www.udemy.com/testing-spring-boot-beginner-to-guru/?couponCode=GITHUB_REPO)
 
--- src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
+++ src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
@@ -2,12 +2,16 @@ package guru.springframework.brewery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-public class TsbbSfgBreweryApplicationTests {
+class TsbbSfgBreweryApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads(ApplicationContext appCtx) {
+        assertThat(appCtx).isNotNull();
     }
 
 }
